6678d46fd8335f26c70b92fd19663a1e0c40e647
Shay2Shay <batterytedd@gmail.com>
2023-03-19 17:08:10 +0545

    Done till home, about, contact

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const sqlite3 = require("sqlite3")
 const express = require('express')
 const path = require('path')
+const bodyparser = require('body-parser')
+const sendIndex = require('./jsData/indexCardDetails')
 
 const databasePath = path.join(__dirname, 'data', 'database.db')
 const db = new sqlite3.Database(databasePath, (err) => {
@@ -11,4 +13,26 @@ const db = new sqlite3.Database(databasePath, (err) => {
     console.log("Database Connected !!!")
 })
 
+const app = express()
+app.use(bodyparser.urlencoded({extended: false}))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(`${__dirname}/public`))
 
+
+app.listen(8000)
+
+// http://localhost:8000/images/crousal1.jpeg
+
+
+app.get('/', (req, res) => {
+    res.render('index', sendIndex)
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
---
+++ jsData/indexCardDetails.js
@@ -0,0 +1,21 @@
+let sendIndex = {
+    image : [
+        {
+            href : "https://farm6.static.flickr.com/5249/5261980632_b7e920ba97_b.jpg",
+            alt : "i1",
+            description : "Some quick example text to build on the card title and make up the bulk of the card's content. 1"
+        },
+        {
+            href : "https://thumbs.dreamstime.com/b/indian-female-shopkeeper-operating-laptop-counter-india-january-indian-female-shopkeeper-operating-laptop-counter-168319621.jpg",
+            alt : "i2",
+            description : "Some quick example text to build on the card title and make up the bulk of the card's content. 2"
+        },
+        {
+            href : "https://i.dailymail.co.uk/i/pix/2010/10/14/article-1320426-0B9BD3DB000005DC-346_634x752.jpg",
+            alt : "i3",
+            description : "Some quick example text to build on the card title and make up the bulk of the card's content. 3"
+        }
+    ]
+}
+
+module.exports = sendIndex
