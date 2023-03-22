3389d294dac3fabe622d659aea6a07d828abf046
Shay2Shay <batterytedd@gmail.com>
2023-03-22 23:22:35 +0545

    last bug fixed. going to submit

--- public/scripts/validation2.js
+++ public/scripts/validation2.js
@@ -1,8 +1,13 @@
 'use strict'
 
 // const form = document.querySelector('form')
-const emai2l = document.getElementById('create-email')
+const email2 = document.getElementById('create-email')
 const password2 = document.getElementById('create-password')
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+
 
 const capitalAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const num = "1234567890".split("")
@@ -28,16 +33,50 @@ function check(mail){
 }
 
 
-document.getElementById('f2').addEventListener('submit', (event) => {
+document.getElementById('f1').addEventListener('submit', (event) => {
     let errs = "";
-    // console.log(password.value.length)
-    // if (!check(email2.value)){
-    //     errs += "Email must be of format xyz@abc.pqr"
-    // }
-    if (password2.value.length <= 5 || password2.value.length >= 21) {
+    console.log(password2.value.length)
+    if (!check(email2.value)){
+        errs += "Email must be of format xyz@abc.pqr"
+    }
+    else if (password2.value.length <= 5 || password2.value.length >= 21) {
         errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n"
     } else {
         let p = password2.value.split('');
+        let a = 0;
+        let b = 0;
+        let c = 0;
+        let d = 0;
+        let final = 1;
+        p.forEach(alpha => {
+            if (capitalAlpha.includes(alpha)) a++;
+            else if (num.includes(alpha)) b++;
+            else if (smallAlpha.includes(alpha)) c++;
+            else if (special.includes(alpha)) d++;
+            else final = 0;
+        });
+
+        if(a*b*c*d*final == 0) errs += "Password Must Have Capital, Small, Numeric and Special Characters (!@#\$%\^\&*\)\(+=._-) only.\n"
+    }
+
+
+    if (errs != "") {
+        alert(errs)
+        event.preventDefault();
+    }
+});
+
+
+document.getElementById('f2').addEventListener('submit', (event) => {
+    let errs = "";
+    console.log(password.value.length)
+    if (!check(email.value)){
+        errs += "Email must be of format xyz@abc.pqr"
+    }
+    if (password.value.length <= 5 || password.value.length >= 21) {
+        errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n"
+    } else {
+        let p = password.value.split('');
         let a = 0;
         let b = 0;
         let c = 0;
