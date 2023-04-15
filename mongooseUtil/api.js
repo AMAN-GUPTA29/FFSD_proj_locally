892d710da366c6ba3923eabe793940ff4b719c3a
percyps2401 <onlystudies2424@gmail.com>
2023-04-15 11:57:40 +0530

    Seller Register in mongoose

--- mongooseUtil/api.js
+++ mongooseUtil/api.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function save(model, instance){
-    return new model(instance).save()
+    return new model(instance).save()//ye promise hai ki aisa hoga jab ye promise poora hoga tabhi then catch chelga
 }
 
 module.exports.save = save
