cb246bfa7c5c2e65b22ad7a8c550b6c41cd95466
AMAN-GUPTA29 <aman.g21@iiits.in>
2023-09-08 10:10:21 +0530

    added ajax to customer registration

--- public/scripts/validation2.js
+++ public/scripts/validation2.js
@@ -1,19 +1,70 @@
-'use strict'
+"use strict";
 
 // const form = document.querySelector('form')
-const email2 = document.getElementById('create-email')
-const password2 = document.getElementById('create-password')
+const email2 = document.getElementById("create-email");
+const password2 = document.getElementById("create-password");
 
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
-
-
-const capitalAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const num = "1234567890".split("")
-const smallAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const special = "!@#\$%\^\&*\)\(+=._-".split('')
-
+const capitalAlpha = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const num = "1234567890".split("");
+const smallAlpha = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const special = "!@#$%^&*)(+=._-".split("");
 
 // function check(mail){
 //     let flag = 0
@@ -32,101 +83,128 @@ const special = "!@#\$%\^\&*\)\(+=._-".split('')
 //     else return false
 // }
 
+function checkr(mail) {
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}$");
+  document.getElementById("email-exist-signin").classList.add("hidden");
 
-function check(mail) {
-    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}$');
-    document.getElementById("email-taken-signin").classList.add('hidden')
+  if (regex.test(mail)) {
+    document
+      .getElementById("email-alertregister-signin")
+      .classList.add("hidden");
 
-    if (regex.test(mail)) {
-        document.getElementById("email-alert-signin").classList.add('hidden')
+    let xhr = new XMLHttpRequest();
 
-        let xhr = new XMLHttpRequest()
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                let res = JSON.parse(xhr.response)
-                if (res.isNotPresent) {
-                    document.getElementById("email-taken-signin").classList.remove('hidden')
-                }
-            }
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        let res = JSON.parse(xhr.response);
+        if (!res.isNotPresent) {
+          document
+            .getElementById("email-exist-signin")
+            .classList.remove("hidden");
         }
+      }
+    };
 
-        xhr.open('GET', `http://localhost:8000/api/customermail/${mail}`)
-        xhr.send()
-        return true
-    }
-    else {
-        return false;
-    }
+    xhr.open("GET", `http://localhost:8000/api/customermail/${mail}`);
+    xhr.send();
+    return true;
+  } else {
+    return false;
+  }
 }
 
-document.getElementById('f1').addEventListener('submit', (event) => {
-    let errs = "";
-    console.log(password2.value.length)
-    if (!check(email2.value)) {
-        errs += "Email must be of format xyz@abc.pqr"
-    }
-    else if (password2.value.length <= 5 || password2.value.length >= 21) {
-        errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n"
-    } else {
-        let p = password2.value.split('');
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        let d = 0;
-        let final = 1;
-        p.forEach(alpha => {
-            if (capitalAlpha.includes(alpha)) a++;
-            else if (num.includes(alpha)) b++;
-            else if (smallAlpha.includes(alpha)) c++;
-            else if (special.includes(alpha)) d++;
-            else final = 0;
-        });
+function check(mail) {
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}$");
+  document.getElementById("email-taken-signin").classList.add("hidden");
 
-        if (a * b * c * d * final == 0) errs += "Password Must Have Capital, Small, Numeric and Special Characters (!@#\$%\^\&*\)\(+=._-) only.\n"
-    }
+  if (regex.test(mail)) {
+    document.getElementById("email-alert-signin").classList.add("hidden");
 
+    let xhr = new XMLHttpRequest();
 
-    if (errs != "") {
-        alert(errs)
-        event.preventDefault();
-    }
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        let res = JSON.parse(xhr.response);
+        if (res.isNotPresent) {
+          document
+            .getElementById("email-taken-signin")
+            .classList.remove("hidden");
+        }
+      }
+    };
+
+    xhr.open("GET", `http://localhost:8000/api/customermail/${mail}`);
+    xhr.send();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+document.getElementById("f1").addEventListener("submit", (event) => {
+  let errs = "";
+  console.log(password2.value.length);
+  if (!check(email2.value)) {
+    errs += "Email must be of format xyz@abc.pqr";
+  } else if (password2.value.length <= 5 || password2.value.length >= 21) {
+    errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n";
+  } else {
+    let p = password2.value.split("");
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+    let final = 1;
+    p.forEach((alpha) => {
+      if (capitalAlpha.includes(alpha)) a++;
+      else if (num.includes(alpha)) b++;
+      else if (smallAlpha.includes(alpha)) c++;
+      else if (special.includes(alpha)) d++;
+      else final = 0;
+    });
+
+    if (a * b * c * d * final == 0)
+      errs +=
+        "Password Must Have Capital, Small, Numeric and Special Characters (!@#$%^&*)(+=._-) only.\n";
+  }
+
+  if (errs != "") {
+    alert(errs);
+    event.preventDefault();
+  }
 });
 
+document.getElementById("f2").addEventListener("submit", (event) => {
+  let errs = "";
+  console.log(password.value.length);
+  if (!check(email.value)) {
+    n;
+    errs += "Email must be of format xyz@abc.pqr";
+  }
+  if (password.value.length <= 5 || password.value.length >= 21) {
+    errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n";
+  } else {
+    let p = password.value.split("");
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+    let final = 1;
+    p.forEach((alpha) => {
+      if (capitalAlpha.includes(alpha)) a++;
+      else if (num.includes(alpha)) b++;
+      else if (smallAlpha.includes(alpha)) c++;
+      else if (special.includes(alpha)) d++;
+      else final = 0;
+    });
 
-document.getElementById('f2').addEventListener('submit', (event) => {
-    let errs = "";
-    console.log(password.value.length)
-    if (!check(email.value)) {
-        n
-        errs += "Email must be of format xyz@abc.pqr"
-    }
-    if (password.value.length <= 5 || password.value.length >= 21) {
-        errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n"
-    } else {
-        let p = password.value.split('');
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        let d = 0;
-        let final = 1;
-        p.forEach(alpha => {
-            if (capitalAlpha.includes(alpha)) a++;
-            else if (num.includes(alpha)) b++;
-            else if (smallAlpha.includes(alpha)) c++;
-            else if (special.includes(alpha)) d++;
-            else final = 0;
-        });
+    if (a * b * c * d * final == 0)
+      errs +=
+        "Password Must Have Capital, Small, Numeric and Special Characters (!@#$%^&*)(+=._-) only.\n";
+  }
 
-        if (a * b * c * d * final == 0) errs += "Password Must Have Capital, Small, Numeric and Special Characters (!@#\$%\^\&*\)\(+=._-) only.\n"
-    }
-
-
-    if (errs != "") {
-        alert(errs)
-        event.preventDefault();
-    }
+  if (errs != "") {
+    alert(errs);
+    event.preventDefault();
+  }
 });
-
-
-
