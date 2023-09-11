88e296e6cb0efcb3a6f536a821d35af51dcf8cc2
someswar177 <someswarasai.p21@iiits.in>
2023-09-11 20:05:50 +0530

    added regex for password in admin

--- public/scripts/registerValidation.js
+++ public/scripts/registerValidation.js
@@ -3,12 +3,12 @@
 // const form = document.querySelector('form')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]{8,}/;
 
-const capitalAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const num = "1234567890".split("")
-const smallAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const special = "!@#\$%\^\&*\)\(+=._-".split('')
-
+const capitalAlpha = /[A-Z]/
+const num = /[0-9]/
+const smallAlpha = /[a-z]/
+const special = /[!@#\$%\^\&*\)\(+=._-]/
 
 function checkEmail(email) {
     let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}$');
@@ -26,10 +26,7 @@ function checkEmail(email) {
                     emailStatusDiv.innerText = "Email is already taken. Please choose another.";
                     emailStatusDiv.classList.remove('hidden');
                 }
-                //  else {
-                //     emailStatusDiv.innerText = "Email is available.";                    
-                //     emailStatusDiv.classList.remove('hidden');
-                // }
+
             }
         };
 
@@ -60,13 +57,10 @@ function checksellermail(email) {
                     emailStatusDiv.innerText = "Email is already taken. Please choose another one.";
                     emailStatusDiv.classList.remove('hidden');
                 }
-                //  else {
-                //     emailStatusDiv.innerText = "Email is available.";                    
-                //     emailStatusDiv.classList.remove('hidden');
-                // }
+                
             }
         };
-
+        
         xhr.open('GET', `http://localhost:8000/api/sellermail/${email}`);
         xhr.send();
         return true;
@@ -77,37 +71,39 @@ function checksellermail(email) {
     }
 }
 
-
 document.addEventListener('submit', (event) => {
     let errs = "";
-    console.log(password.value.length)
-    if (!checkEmail(email.value)){
-        errs += "Email must be of format xyz@abc.pqr"
-    }
-    else 
-    if (password.value.length <= 5 || password.value.length >= 21) {
-        errs += "Password Must Be At Least Of Length 6 and Max Length Can Be 20.\n"
-    } else {
-        let p = password.value.split('');
-        let a = 0;
-        let b = 0;
-        let c = 0;
-        let d = 0;
-        let final = 1;
-        p.forEach(alpha => {
-            if (capitalAlpha.includes(alpha)) a++;
-            else if (num.includes(alpha)) b++;
-            else if (smallAlpha.includes(alpha)) c++;
-            else if (special.includes(alpha)) d++;
-            else final = 0;
-        });
 
-        if(a*b*c*d*final == 0) errs += "Password Must Have Capital, Small, Numeric and Special Characters (!@#\$%\^\&*\)\(+=._-) only.\n"
+    if (!checkEmail(email.value)) {
+        errs += "Email must be of format xyz@abc.pqr\n";
     }
 
+    if (password.value.length < 6 || password.value.length > 20) {
+        errs += "Password must be between 6 and 20 characters in length.\n";
+    } else if (!passwordRegex.test(password.value)) {
+        errs += "Password Must Have Capital, Small, Numeric, and Special Characters (!@#$%^&*)(+=._-) only.\n";
+    }
 
-    if (errs != "") {
-        alert(errs)
+    if (errs !== "") {
+        alert(errs);
         event.preventDefault();
+    } else {
+        const form = document.querySelector(".form");
+        const submitButton = document.querySelector("#submitButton");
+
+        event.preventDefault();
+        const loading = document.querySelector("#loading");
+        loading.classList.remove("d-none");
+        const delay = 800; 
+        setTimeout(function () {
+            loading.classList.add("d-none");
+            form.submit();
+        }, delay);
     }
 });
+
+
+
+
+  
+  
